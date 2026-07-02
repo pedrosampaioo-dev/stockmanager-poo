3952a2f21023ee28fa31e6f5cfc91e6cb8c41d09
pedrosampaioo-dev <pedrohss717@gmail.com>
2026-07-02 18:52:41 -0300

    docs: geração da documentação Javadoc para todas as classes do projeto

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"stockmanager"},{"l":"stockmanager.exception"},{"l":"stockmanager.interfaces"},{"l":"stockmanager.model"}]
